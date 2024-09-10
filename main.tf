4048411e1ee73200b5c265952a558428f36ef845
Asfand Qazi <ayqazi@gmail.com>
2024-09-10 11:56:52 +0100

    Standardize data sources, locals, and tags

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
 locals {
+  account_id = data.aws_caller_identity.current.account_id
+  aws_region = data.aws_region.current.name
+
   mlflow_bucket_name = "${var.bucket_prefix}-mlflow"
+
+  tags = {
+    Component = "apps"
+  }
 }
 
 module "bucket_mlflow" {
@@ -7,4 +18,6 @@ module "bucket_mlflow" {
   version = "~> 4.1"
 
   bucket = local.mlflow_bucket_name
+
+  tags = local.tags
 }
